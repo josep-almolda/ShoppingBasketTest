0d26bc4a9b62fe8a8227ad95e89dd332f7e843db
josep <jmalmolda@gmail.com>
2019-01-30 21:55:19 +0000

    add unit tests for shopping basket manager

--- ShoppingBasket/ShoppingBasketManager.cs
+++ ShoppingBasket/ShoppingBasketManager.cs
@@ -1,10 +1,13 @@
 ﻿using ShoppingBasket.Models;
 using System;
+using System.Collections.Generic;
 
 namespace ShoppingBasket
 {
     public class ShoppingBasketManager
     {
+        public List<Product> ProductsList { get; private set; }
+
         public void AddProduct(Product product)
         {
             throw new NotImplementedException();
---
+++ ShoppingBasketTests/UnitTests/ShoppingBasketManagerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using ShoppingBasket;
+using ShoppingBasket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingBasketTests.UnitTests
+{
+    [TestClass]
+    public class ShoppingBasketManagerTests
+    {
+        [TestMethod]
+        public void WhenFirstProductIsAddedTheListIsUpdated()
+        {
+            // Arrange
+            var systemUnderTest = new ShoppingBasketManager();
+
+            // Act
+            systemUnderTest.AddProduct(new Product(ProductType.Bread));
+
+            // Assert
+            Check.That(systemUnderTest.ProductsList.Count).Equals(1);
+            Check.That(systemUnderTest.ProductsList.First().Type).Equals(ProductType.Bread);
+        }
+
+        [TestMethod]
+        public void WhenProductIsAddedToExistingListProductIsAddedLast()
+        {
+            // Arrange
+            var systemUnderTest = new ShoppingBasketManager();
+            systemUnderTest.AddProduct(new Product(ProductType.Butter));
+            systemUnderTest.AddProduct(new Product(ProductType.Bread));
+            systemUnderTest.AddProduct(new Product(ProductType.Milk));
+
+            // Act
+            systemUnderTest.AddProduct(new Product(ProductType.Bread));
+
+            // Assert
+            Check.That(systemUnderTest.ProductsList.Count).Equals(4);
+            Check.That(systemUnderTest.ProductsList[0].Type).Equals(ProductType.Butter);
+            Check.That(systemUnderTest.ProductsList[1].Type).Equals(ProductType.Bread);
+            Check.That(systemUnderTest.ProductsList[2].Type).Equals(ProductType.Milk);
+            Check.That(systemUnderTest.ProductsList[3].Type).Equals(ProductType.Bread);
+        }
+    }
+}
