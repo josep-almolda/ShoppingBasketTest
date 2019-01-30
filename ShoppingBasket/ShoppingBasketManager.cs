a42011f6155b48753cb65adc374a588f57d5147a
josep <jmalmolda@gmail.com>
2019-01-30 21:57:48 +0000

    implement AddProduct method to pass the tests

--- ShoppingBasket/ShoppingBasketManager.cs
+++ ShoppingBasket/ShoppingBasketManager.cs
@@ -6,11 +6,11 @@ namespace ShoppingBasket
 {
     public class ShoppingBasketManager
     {
-        public List<Product> ProductsList { get; private set; }
+        public List<Product> ProductsList { get; private set; } = new List<Product>();
 
         public void AddProduct(Product product)
         {
-            throw new NotImplementedException();
+            ProductsList.Add(product);
         }
 
         public decimal CalculateTotal()
